3024855818e6091ad07bf5342bc49b922a664947
Nicolás Fierro <nicolas.fierro@ug.uchile.cl>
2022-11-16 13:30:27 -0300

    new error, different test

--- examples/symregv3.py
+++ examples/symregv3.py
@@ -1,6 +1,6 @@
 import random
 from numpy import cos, sin
-from sge.utilities.protected_math import _log_, _div_, _exp_, _inv_, _sqrt_, protdiv
+from sge.utilities.protected_math import _log_, _div_, _exp_, _sig_, _inv_, _sqrt_, protdiv
 import pandas as pd
 
 
@@ -84,7 +84,7 @@ class SymbolicRegression():
             #    l.append([xx,yy])
 
             #self.__train_set = l
-            df = pd.read_csv('dataset/Total/TrainTotal0.txt', sep=",",header=None)
+            df = pd.read_csv('dataset/Total/TrainSPM.txt', sep=",",header=None)
             l=df.to_numpy().tolist()
             self.__train_set =l
             self.training_set_size = len(self.__train_set)
@@ -93,7 +93,7 @@ class SymbolicRegression():
                 #function = eval(self.function)
                 #yy = map(function, xx)
                 print('Leyendo test de proyecto....')
-                df = pd.read_csv('dataset/Total/TrainTotal0.txt', sep=",",header=None)
+                df = pd.read_csv('dataset/Total/TrainSPM.txt', sep=",",header=None)
                 l=df.to_numpy().tolist()
                 self.__test_set = l
                 self.test_set_size = len(self.__test_set)
@@ -120,6 +120,7 @@ class SymbolicRegression():
 
     def get_error(self, individual, dataset):
         pred_error = 0
+        cuociente= 0.00001
         maxData=0
         posData=0
         maxPred=0
@@ -131,8 +132,8 @@ class SymbolicRegression():
             try:
 
                 result = eval(individual, globals(), {"x": fit_case[:-1]})
-                pred_error += (case_output - result)**2
-
+                pred_error += (case_output - result)**2/case_output
+                cuociente += result**2
                 if maxData<case_output:
                     maxData=case_output
                     posData=kij
@@ -145,9 +146,9 @@ class SymbolicRegression():
             except (OverflowError, ValueError) as e:
                 return self.__invalid_fitness
 
-            abcd=_sqrt_((maxData-maxPred)**2+(posData-posPred)**2)
-            eror_=_sqrt_(pred_error)+abcd/kij
-        return eror_
+            #abcd=_sqrt_((maxData-maxPred)**2+(posData-posPred)**2)
+            eror_=_sqrt_(pred_error)/_sqrt_(cuociente)
+        return 100*eror_
 
     def evaluate(self, individual):
         error = 0.0
